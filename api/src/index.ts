a556a3ddaf70b5cb18fe2588b15b64bdd0ab9eb6
AlexLeunens <alexandre.leunens@gmail.com>
2020-12-09 17:00:18 +0100

    mapped posts api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,8 @@ import { createConnection } from "typeorm"
 import { User } from "./entity/User"
 
 import UserRepository from './repository/userRepository'
+import PostRepository from "./repository/postRepository";
+import { Post } from "./entity/Post";
 
 createConnection().then(async connection => {
 
@@ -20,12 +22,59 @@ createConnection().then(async connection => {
         next()
     })
 
+    // Users
     app.get('/user/all', async (req, res) => {
         const repo = new UserRepository();
         const result = await repo.all()
 
         if (result) {
             res.status(200).json(result)
+        }
+
+        res.end()
+    })
+
+    // Posts
+    app.get('/post/all', async (req, res) => {
+        const repo = new PostRepository();
+        const result = await repo.all()
+
+        if (result) {
+            res.status(200).json(result)
+        }
+
+        res.end()
+    })
+
+    app.post('/post/save', async (req, res) => {
+        const title = req.body.title
+        const content = req.body.content
+        const date = req.body.date
+
+        const newPost = new Post()
+        newPost.title = title;
+        newPost.content = content;
+        newPost.date = date;
+
+        const repo = new PostRepository();
+        const result = await repo.save(newPost)
+
+        if (result) {
+            res.status(200).json(result)
+        }
+
+        res.end()
+    })
+
+    app.delete('post/delete/:id', async (req, res) => {
+        const id = req.params.id
+        const repo = new PostRepository();
+
+        try {
+            await repo.remove(id)
+            res.status(204)
+        } catch (e) {
+            res.status(500)
         }
 
         res.end()
